a9b1af4e7f0331bbcd2a7a18d5adb8883c24d20a
Yaroslav <122485322+darkangel1403@users.noreply.github.com>
2026-04-20 18:05:27 +0300

    Create AppointmentStatus.cs

---
+++ Models/AppointmentStatus.cs
@@ -0,0 +1,12 @@
+namespace MedHelp.Models
+{
+    public enum AppointmentStatus
+    {
+        Free = 0,
+        Booked = 1,
+        Confirmed = 2,
+        InProgress = 3,
+        Completed = 4,
+        Cancelled = 5
+    }
+}
